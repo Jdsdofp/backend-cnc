17787002eb5296c8d6bfbc5ceef102caa0560461
unknown <jdsdofp@gmail.com>
2021-10-28 18:22:19 -0300

    Alteração no model spot

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+require("dotenv").config()
 
 const SpotSchema = new mongoose.Schema({
     thumbnail: String,
@@ -16,7 +17,7 @@ const SpotSchema = new mongoose.Schema({
 });
 
 SpotSchema.virtual('thumbnail_url').get(function() {
-    return `http://localhost:3333/files/${this.thumbnail}`
+    return `${process.env.APP_URL}/files/${this.thumbnail}`
 })
 
 module.exports = mongoose.model('Spot', SpotSchema);
