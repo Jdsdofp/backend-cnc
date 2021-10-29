0c95d0e2ca8b233fb56fc1d4133cd30182604dca
unknown <jdsdofp@gmail.com>
2021-10-29 12:28:36 -0300

    Sokcet

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const io = require('socket.io')(server, {
         origin: "*",
         methods: ["GET", "POST"],
         allowedHeaders: ["my-custom-header"],
-        credentials: true
+        credentials: false
     }
 });
 
